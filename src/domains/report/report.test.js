1fa1f0023fa09c4e6965d56348151188cc581c88
Jorge Franzon <jorgefzrossi@gmail.com>
2019-09-09 17:04:50 -0300

    Adicionando FIREBASE_CONFIG no CircleCI

--- src/domains/report/report.test.js
+++ src/domains/report/report.test.js
@@ -1,6 +1,6 @@
-describe("report", () => {
-  describe("addReport", () => {
-    it("example test", async () => {
+describe('report', () => {
+  describe('addReport', () => {
+    it('example test', async () => {
       expect(1 + 1).toBe(2);
     });
   });
--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,7 +1,7 @@
-const admin = require("firebase-admin");
-const { FIREBASE_CONFIG, STORAGE_BUCKET_URL } = require("../env");
+const admin = require('firebase-admin');
+const { FIREBASE_CONFIG, STORAGE_BUCKET_URL } = require('../env');
 
 admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(FIREBASE_CONFIG)),
-  storageBucket: STORAGE_BUCKET_URL
+  storageBucket: STORAGE_BUCKET_URL,
 });
